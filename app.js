f85bfc96e4970b1a010ddba5a62e8d24c7488fc7
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-08-14 01:27:51 +0500

    function

---
+++ app.js
@@ -0,0 +1,25 @@
+function greetUser(firstName, lastName) {
+      const fullName = `${firstName} ${lastName}`;
+      console.log(`Hello, ${fullName}! Welcome.`);
+}
+
+// Example usage
+const userFirstName = prompt("enter your name");
+const userLastName = prompt("enter your name");
+greetUser(userFirstName, userLastName);
+
+
+
+
+// let calculate_square(number);
+// square = number ** 2
+// return square
+
+// # Example usage
+// num1 = 5
+// result1 = calculate_square(num1)
+// print(f"The square of {num1} is {result1}")
+
+// num2 = 8
+// result2 = calculate_square(num2)
+// print(f"The square of {num2} is {result2}")
